b7ebd7ca3124090f80643e0ac897da7f7bdbd1a5
Carlos Augusto <carlosflores@motumweb.com>
2017-05-26 15:45:26 +0000

    se crea proyecto y commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-prueba',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  tituloPrincipal = 'Primer proyecto de prueba con Angular2!';
+  tituloCard00 = 'Tarjeta 00';
+  descripcionCard00 = 'Descripción 00';
+  textoBotonCar00 = 'Boton00';
+  tituloCard01 = 'Tarjeta 01';
+  descripcionCard01 = 'Descripción 01';
+  textoBotonCar01 = 'Boton01';
+
+  actionBoton00(): void {
+    console.log("se ha presionado el boton00");
+    this.tituloCard00 = this.tituloCard00.toUpperCase();
+  }
+  actionBoton01(): void {
+    console.log("se ha presionado el boton01");
+    this.tituloCard01 = this.tituloCard01.toUpperCase();
+  }
+
+
 }
